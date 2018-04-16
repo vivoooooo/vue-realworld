c09ef9c44de73aa104b70a14e1b3a45f998a9d64
vivian li <fashpashh@gmail.com>
2018-04-16 16:20:22 +1000

    Header component done and updated html head

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
   name: 'App',
+  components: { Header },
 };
 </script>
 
